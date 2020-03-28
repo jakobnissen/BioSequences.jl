3357e2c9cc1f369f858d5cb73d4ee67aac352875
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2020-03-28 12:33:14 +0100

    Added fast hashing

--- src/mers/predicates.jl
+++ src/mers/predicates.jl
@@ -5,3 +5,7 @@
 Base.cmp(seq1::T, seq2::T) where {T<:AbstractMer} = cmp(encoded_data(seq1), encoded_data(seq2))
 Base.:(==)(x::T, y::T) where {T<:AbstractMer} = encoded_data(x) == encoded_data(y)
 Base.isless(x::T, y::T) where {T<:AbstractMer} = isless(encoded_data(x), encoded_data(y))
+
+function Base.hash(x::Mer{<:Union{DNAAlphabet{N}, RNAAlphabet{N}},K}, h::UInt) where {N,K}
+    return Base.hash_uint64(encoded_data(x) ⊻ K ⊻ N)
+end
